b5ffad8d0bd65e761525fcb8eadd3faab16b5ff2
62theories <s5935512005@phuket.psu.ac.th>
2018-10-03 04:27:53 +0700

    check point 1

---
+++ wt-app/components/Forecast.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  ScrollView,
+  Text,
+  View,
+  ImageBackground,
+} from 'react-native';
+export default class Forecast extends React.Component {
+ render() {
+ return (
+ <View>
+ <Text>{this.props.main}</Text>
+ <Text>{this.props.description}</Text>
+ <Text>{this.props.temp}</Text>
+ <Text>°C</Text>
+ </View>
+ );
+ }
+}
--- wt-app/components/Weather.js
+++ wt-app/components/Weather.js
@@ -1,30 +1,36 @@
-import React from 'react'; 
-import { StyleSheet, Text, View } from 'react-native'; 
- 
-export default class Weather extends React.Component 
-{   
-  doIt = () => 
-  {     
-    console.log("Hello from console")   
-  }   
-  render() 
-  {     
-    return (       
-      <View style={styles.container}>         
-        <Text>
-          {this.props.zipCode}
-        </Text>         
-      </View>    
-          );   
-  } 
-} 
- 
-const styles = StyleSheet.create({   
-                                  container: 
-                                  {     
-                                    flex: 1,     
-                                    backgroundColor: '#fff',     
-                                    alignItems: 'center',     
-                                    justifyContent: 'center',   
-                                  }, 
-                                }); 
+import React, {Component}  from 'react';
+import Forecast from './Forecast';
+import {
+  AppRegistry,
+  StyleSheet,
+  ScrollView,
+  Text,
+  View,
+  ImageBackground,
+} from 'react-native';
+export default class Weather extends React.Component {
+ constructor(props) {
+ super(props);
+ this.state = {
+ forecast: {
+ main: '-', description: '-', temp: 0
+ }
+ }
+ }
+ render() {
+ return (
+ <View style={styles.container}>
+ <ImageBackground source={require('../a.jpg')} style={styles.backdrop}>
+ <Text>Zip code is {this.props.zipCode}.</Text>
+ <Forecast {...this.state.forecast} />
+ </ImageBackground>
+ </View>
+ );
+ }
+}
+
+
+const styles = StyleSheet.create({
+ container: { paddingTop: 25 },
+ backdrop: { width: '100%', height: '100%'},
+});
